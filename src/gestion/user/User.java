f38ad934213f616e3fdf0c6f19910a401d7bea55
HAMADOU BA <hamaba2016@gmail.com>
2023-12-26 19:58:00 +0000

    v0.0

---
+++ src/gestion/user/User.java
@@ -0,0 +1,12 @@
+package gestion.user;
+
+public class User {
+    private int id;
+    private int id_role;
+    private String email;
+    private String date_created;
+    private String date_last_edit;
+    private String password;
+    private String password_hashed;
+
+}
